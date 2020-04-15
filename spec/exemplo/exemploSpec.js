8b15fe973fddebc490c50be613aef8683b742429
Roberto Lima <robertolima@MacBook-Pro-de-Roberto.local>
2020-04-15 09:02:33 -0300

    node com jasmine e karma

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,10 @@
+describe("Suite de teste de exemplo com NodeJS", () => {
+
+    it("Deve demonstrar o uso do jasmine com NodeJS", () => {
+        expect(true).toEqual(true)
+    })
+
+    it("Deve demonstrar o uso do Karma em modo de escuta", () => {
+        expect(1).toEqual(1)
+    })
+})
